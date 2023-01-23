7bc31b7b9a20c8660d330d4939bbe51f91eb1eaf
Gabriel Pinheiro <gabrielpsilvas@live.com>
2023-01-23 12:10:23 -0300

    chore(setup): added tailwidcss initial setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from '@/app'
-import './index.css'
+import '@/styles/globals.css'
 
 const rootElement = document.querySelector('[data-js="root"]')
 
