30bfac0f02166c4d4e855d89449fba252a90e746
Gabriel Pinheiro <gabrielpsilvas@live.com>
2023-01-24 16:31:55 -0300

    feat(app): added button to connect and disconnect in MetaMask wallet

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,11 @@
 import { Button, Checkbox, Input } from '@/components'
 import { ReactComponent as Logo } from '@/assets/logo.svg'
 
+import { metaMask, hooks } from '@/connectors/metamask'
+import { useEffect } from 'react'
+
+const { useAccount, useIsActive } = hooks
+
 function NewTask () {
   return (
     <div>
@@ -28,9 +33,36 @@ function TaskItem () {
 }
 
 export function App () {
+  const account = useAccount()
+  const isActive = useIsActive()
+
+  const handleConnect = async () => await metaMask.activate()
+  const handleDisconnect = async () => {
+    if (metaMask.deactivate) {
+      await metaMask.deactivate()
+    } else {
+      await metaMask.resetState()
+    }
+  }
+
+  useEffect(() => {
+    (async () => {
+      await metaMask.connectEagerly()
+    })()
+  }, [])
+
   return (
     <main className='max-w-[635px] w-full flex flex-col'>
-      <div className='pt-14 flex justify-center'>
+      <div className='absolute right-10 flex gap-6 items-center'>
+        <p className='text-white'>{account}</p>
+        <Button
+          variant={isActive ? 'secondary' : 'primary'}
+          onClick={isActive ? handleDisconnect : handleConnect}
+        >
+          {isActive ? 'Disconnect' : 'Connect'}
+        </Button>
+      </div>
+      <div className='pt-20 flex justify-center'>
         <Logo />
       </div>
       <NewTask />
--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -3,12 +3,18 @@ import { ReactNode, ButtonHTMLAttributes } from 'react'
 interface ButtonProps
   extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'className'> {
   children: ReactNode;
+  variant?: 'primary' | 'secondary';
 }
 
-export function Button ({ children, ...props }: ButtonProps) {
+export function Button ({
+  children,
+  variant = 'primary',
+  ...props
+}: ButtonProps) {
+  const bgColor = variant === 'primary' ? 'bg-primary' : 'bg-red-500'
   return (
     <button
-      className='h-10 bg-primary rounded text-white flex items-center justify-center px-4 py-2 min-w-[105px] font-bold text-base hover:bg-primaryDark transition-all -tracking-[0.035em] outline-none focus:outline-none focus:ring focus:ring-primaryDark'
+      className={`${bgColor} h-10 rounded text-white flex items-center justify-center px-4 py-2 min-w-[105px] font-bold text-base hover:bg-primaryDark transition-all -tracking-[0.035em] outline-none focus:outline-none focus:ring focus:ring-primaryDark`}
       {...props}
     >
       {children}
